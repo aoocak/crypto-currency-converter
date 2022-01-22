0e070cd03ba6bd0c0d546d47b5de723b689c47bd
Ahmet Orhan Ocak <ahmetorhanocak@Ahmets-MacBook-Pro-2.local>
2022-01-22 18:17:16 +0300

    setups maven project

---
+++ src/main/java/com/zoopluscase/cryptocurrencyconverter/CryptoCurrencyConverterApplication.java
@@ -0,0 +1,13 @@
+package com.zoopluscase.cryptocurrencyconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoCurrencyConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptoCurrencyConverterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zoopluscase/cryptocurrencyconverter/CryptoCurrencyConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoopluscase.cryptocurrencyconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoCurrencyConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
