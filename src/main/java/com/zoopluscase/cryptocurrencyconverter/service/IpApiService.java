83eacb84b7a59f7ab7d6d639dd4bedbcb5609f49
Ahmet Orhan Ocak <ahmetorhanocak@Ahmets-MacBook-Pro-2.local>
2022-01-23 19:52:52 +0300

    implements geolocation service

---
+++ src/main/java/com/zoopluscase/cryptocurrencyconverter/service/IpApiService.java
@@ -0,0 +1,23 @@
+package com.zoopluscase.cryptocurrencyconverter.service;
+
+import com.zoopluscase.cryptocurrencyconverter.client.IpApiClient;
+import com.zoopluscase.cryptocurrencyconverter.model.GeoLocation;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+@Service
+public class IpApiService {
+
+    private final IpApiClient ipApiClient;
+
+    public IpApiService(IpApiClient ipApiClient) {
+        this.ipApiClient = ipApiClient;
+    }
+
+    public GeoLocation getGeoLocation(String ipAddress) throws URISyntaxException, IOException {
+        GeoLocation geoLocation = ipApiClient.getGeoLocation(ipAddress);
+        return geoLocation;
+    }
+}
