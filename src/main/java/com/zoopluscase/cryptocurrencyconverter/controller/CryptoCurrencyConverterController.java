cafff944c38730295d143f1778b359147c8cf8dc
Ahmet Orhan Ocak <ahmetorhanocak@Ahmets-MacBook-Pro-2.local>
2022-01-23 20:30:42 +0300

    implements controller

--- src/main/java/com/zoopluscase/cryptocurrencyconverter/controller/CryptoCurrencyConverterController.java
+++ src/main/java/com/zoopluscase/cryptocurrencyconverter/controller/CryptoCurrencyConverterController.java
@@ -1,13 +1,20 @@
 package com.zoopluscase.cryptocurrencyconverter.controller;
 
+import com.zoopluscase.cryptocurrencyconverter.client.coinmarketcap.CoinMarketCapClientException;
+import com.zoopluscase.cryptocurrencyconverter.client.ipapi.IpApiClientException;
 import com.zoopluscase.cryptocurrencyconverter.model.ConvertRequestDTO;
+import com.zoopluscase.cryptocurrencyconverter.model.ConvertResponseDTO;
 import com.zoopluscase.cryptocurrencyconverter.service.CryptoCurrencyConversionService;
+import com.zoopluscase.cryptocurrencyconverter.service.coinmarketcap.CoinMarketCapServiceException;
+import com.zoopluscase.cryptocurrencyconverter.service.ipapi.IpApiValidationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 @Controller
 public class CryptoCurrencyConverterController {
@@ -19,7 +26,21 @@ public class CryptoCurrencyConverterController {
     }
 
     @GetMapping("/convert")
-    public String convert(@ModelAttribute ConvertRequestDTO requestDTO, HttpServletRequest httpServletRequest, Model model) {
-        return "";
+    public String convert(@ModelAttribute ConvertRequestDTO requestDTO, HttpServletRequest httpServletRequest, Model model)
+            throws IpApiClientException, CoinMarketCapClientException, IpApiValidationException, URISyntaxException, IOException, CoinMarketCapServiceException {
+
+        model.addAttribute("requestDTO", requestDTO);
+        ConvertResponseDTO responseDTO = new ConvertResponseDTO();
+
+        // landing
+        if (requestDTO.getCoinSymbol() == null) {
+            model.addAttribute("responseDTO", responseDTO);
+            return "convert";
+        }
+
+        responseDTO = cryptoCurrencyConversionService.convertCryptoCurrency(requestDTO, httpServletRequest);
+
+        model.addAttribute("responseDTO", responseDTO);
+        return "convert";
     }
 }
